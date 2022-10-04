6dd38b9b38bd12d0722d3cd3f8879a54cd8676cd
Amacovema <amandaptx118@gmail.com>
2022-10-04 18:44:28 -0500

    se agrego range.java

---
+++ Parcial1/tarea3/Flag.java
@@ -0,0 +1,16 @@
+/*
+*Flag.java
+*Author: adsoft
+*Date: Oct 4, 2022
+*Location: Ixtac, Ver. Mexico
+*/
+
+class Flag { // name of Program
+  public static void main(String[] args) { // main
+
+    boolean flag = false;        // define a boolean var
+    System.out.println("Valor de flag: ");
+    System.out.println(!!flag);   //prints true
+    }
+}
+
