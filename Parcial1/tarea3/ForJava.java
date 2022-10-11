34b1ae274c3ff3395ce0443649cd25e039d72c4b
Amacovema <amandaptx118@gmail.com>
2022-10-11 18:36:37 -0500

    add if, switch,for

---
+++ Parcial1/tarea3/ForJava.java
@@ -0,0 +1,13 @@
+public interface ForJava {
+// Program to print a text 5 times
+
+  public static void main(String[] args) {
+
+    int n = 5;
+    // for loop  
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(i);
+      System.out.print("Java is fun");
+    }
+  }
+}
